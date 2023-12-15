736d5d38a474f41fb934de99a89657831270790c
Manuel velasco <manuelvelasco2020@gmail.com>
2023-12-14 22:23:10 -0800

    trying to understand project structure and flow of the code

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log('Rock Paper Scissors');
+
+function getComputerChoice() {
+  // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+}
+
+function playerSelection() {
+  /*
+		Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation)
+	*/
+}
+
+// maybe need a function
+// function computerSelection() {}
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  //
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  /* 
+	best-of-five game that keeps score and reports a winner or loser at the end.
+
+	*/
+}
+
+// call playRound function 5 times in a row.
