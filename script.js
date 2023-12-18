76864fc83e489fdc0c2e9ea26051ef4d819500d4
Manuel velasco <manuelvelasco2020@gmail.com>
2023-12-17 21:12:58 -0800

    function getComputerChoice randomly return either rock paper or scissors

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-console.log('Rock Paper Scissors');
-
 function getComputerChoice() {
+  let rockPaperScissor = ['Rock', 'Paper', 'Scissor'];
+
+  let randomlyReturnRPS =
+    rockPaperScissor[Math.floor(Math.random() * rockPaperScissor.length)];
+
+  return randomlyReturnRPS;
   // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 }
 
+console.log(getComputerChoice());
+
 function playerSelection() {
+  let playerWord = prompt('enter Rock, Paper or Scissors');
+
+  let playerWordCaseInsen = playerWord.toLowerCase();
+  return (
+    playerWordCaseInsen.charAt(0).toUpperCase() + playerWordCaseInsen.slice(1)
+  );
+
   /*
 		Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation)
 	*/
 }
+console.log(playerSelection());
 
 // maybe need a function
 // function computerSelection() {}
@@ -18,10 +32,9 @@ function playRound(playerSelection, computerSelection) {
   //
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
+// const playerSelection = 'rock';
+// const computerSelection = getComputerChoice();
+//
 function game() {
   /* 
 	best-of-five game that keeps score and reports a winner or loser at the end.
