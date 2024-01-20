85850aa1fc62e85cc4611cef1f54e83d2be38515
Manuel velasco <manuelvelasco2020@gmail.com>
2024-01-20 15:27:01 -0800

    playground function is not complete, still working on the logic

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
+// computer return randomly  either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice() {
-  let rockPaperScissor = ['Rock', 'Paper', 'Scissor'];
+  let rockPaperScissor = ['Rock', 'Paper', 'Scissors'];
 
-  let randomlyReturnRPS =
+  let returnRandomlyWord =
     rockPaperScissor[Math.floor(Math.random() * rockPaperScissor.length)];
 
-  return randomlyReturnRPS;
-  // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+  return returnRandomlyWord;
 }
 
-console.log(getComputerChoice());
-
+// case-insensitive (so users can input rock, ROCK, RocK or any other variation)
 function playerSelection() {
   let playerWord = prompt('enter Rock, Paper or Scissors');
 
-  let playerWordCaseInsen = playerWord.toLowerCase();
-  return (
-    playerWordCaseInsen.charAt(0).toUpperCase() + playerWordCaseInsen.slice(1)
-  );
-
-  //   /*
-  // 		Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation)
-  // 	*/
+  let playerWordCase = playerWord.toLowerCase();
+  return playerWordCase.charAt(0).toUpperCase() + playerWordCase.slice(1);
 }
-console.log(playerSelection());
-
-// maybe need a function
-// function computerSelection() {}
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-  //
-}
-
-// const playerSelection = 'rock';
-// const computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
 //
-function game() {
-  /* 
-	best-of-five game that keeps score and reports a winner or loser at the end.
+//
+//
+//
 
-	*/
+// declares the winner of the round using a string
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === 'Paper') === computerSelection ||
+    (playerSelection === 'Rock') === computerSelection ||
+    (playerSelection === 'Scissors') === computerSelection
+  ) {
+    return `you win, ${playerSelection} beats ${computerSelection}`;
+  } else if (
+    (computerSelection === 'Paper') === playerSelection ||
+    (computerSelection === 'Rock') === playerSelection ||
+    (computerSelection === 'Scissors') === playerSelection
+  ) {
+    return `you lose ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection === computerSelection) {
+    return `draw`;
+  }
 }
 
-// call playRound function 5 times in a row.
+//
+//
+//
+//
+console.log(playRound(playerSelection(), computerSelection));
+
+// best-of-five game that keeps score and reports a winner or loser at the end.
+// function game() {
+
+// }
